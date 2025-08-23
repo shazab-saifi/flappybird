e9e5b20ee73ee2382d90445ef449e7b79523cf69
shazab-saifi <shazebsaifi92@gmail.com>
2025-08-23 13:22:42 +0530

    Implemented game over, falling, and re-start logic

--- components/Game.tsx
+++ components/Game.tsx
@@ -6,6 +6,23 @@ const Game = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [start, setStart] = useState<boolean>(true);
   const frameIdRed = useRef<number | null>(null);
+  const [showButton, setShowButton] = useState<boolean>(true);
+  const birdImgYRef = useRef<number>(200);
+  const [gameOver, setGameOver] = useState(false);
+
+  const buttonClasses =
+    'cursor-pointer w-fit rounded-xl bg-orange-600 px-4 py-2 text-center text-xl font-bold text-white ring-2 ring-white transition-colors hover:bg-orange-500 active:bg-orange-400';
+
+  // const handler = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  //   e.preventDefault();
+  //   birdImgYRef.current -= 5;
+  // };
+
+  useEffect(() => {
+    if (gameOver && frameIdRed.current !== null) {
+      cancelAnimationFrame(frameIdRed.current);
+    }
+  }, [gameOver]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -57,7 +74,6 @@ const Game = () => {
     const tileW = 30;
     const spacing = 36;
     const groundY = 572;
-    const birdImgY = 200;
     let birdAngle = 0;
 
     async function animate() {
@@ -69,13 +85,26 @@ const Game = () => {
 
       ctx.drawImage(backgroundImg, 0, 0, canvasRef.current.width, canvasRef.current.height);
 
+      ctx.drawImage(
+        birdImg,
+        200,
+        !showButton ? (birdImgYRef.current += 5) : birdImgYRef.current + birdOffset,
+        80,
+        80
+      );
+
       ctx.fillStyle = 'green';
       ctx.fillRect(0, 570, canvasRef.current.width, 30);
 
       ctx.fillStyle = 'rgba(216, 195, 95, 1)';
       ctx.fillRect(0, 600, canvasRef.current.width, 100);
 
-      ctx.drawImage(birdImg, 200, birdImgY + birdOffset, 80, 80);
+      if (birdImgYRef.current === 520) {
+        setGameOver(true);
+        if (frameIdRed.current !== null) {
+          cancelAnimationFrame(frameIdRed.current);
+        }
+      }
 
       if (tileOffset <= -spacing) tileOffset += spacing;
 
@@ -90,33 +119,52 @@ const Game = () => {
     }
 
     animate();
-  }, [start]);
-
-  useEffect(() => {}, []);
+  }, [start, showButton]);
 
   return (
-    <div className="relative">
-      <canvas ref={canvasRef} width={500} height={700} className="bg-black" />
-      <div className="absolute top-0 right-0 inline-flex gap-4">
-        <button
-          onClick={() => setStart(true)}
-          className="cursor-pointer rounded-md bg-orange-600 px-4 py-2 text-center font-bold text-white"
-        >
-          Start Game
-        </button>
-        <button
-          onClick={() => {
-            if (frameIdRed.current !== null) {
-              cancelAnimationFrame(frameIdRed.current);
-              frameIdRed.current = null;
-              setStart(false);
-            }
-          }}
-          className="cursor-pointer rounded-md bg-orange-600 px-4 py-2 text-center font-bold text-white"
-        >
-          Stop Game
-        </button>
-      </div>
+    <div className="relative flex w-full flex-col items-center justify-center">
+      <canvas
+        // onClick={(e) => handler(e)}
+        ref={canvasRef}
+        width={500}
+        height={700}
+        className="bg-black"
+      />
+      {showButton && (
+        <div className="absolute top-100 mx-auto inline-flex gap-4">
+          <button
+            onClick={() => {
+              setStart(true);
+              setShowButton(false);
+            }}
+            className={buttonClasses}
+          >
+            Start Game
+          </button>
+          <button
+            onClick={() => {
+              if (frameIdRed.current !== null) {
+                cancelAnimationFrame(frameIdRed.current);
+                frameIdRed.current = null;
+                setStart(false);
+              }
+            }}
+            className={buttonClasses}
+          >
+            Stop Game
+          </button>
+        </div>
+      )}
+      {gameOver && (
+        <div className="absolute top-100 mx-auto flex flex-col items-center justify-center gap-4">
+          <div className="rounded-2xl bg-orange-600 px-8 py-4 text-center text-4xl font-bold text-white ring-2 ring-white">
+            Game Over
+          </div>
+          <button onClick={() => setStart(true)} className={buttonClasses}>
+            Re-Start
+          </button>
+        </div>
+      )}
     </div>
   );
 };
